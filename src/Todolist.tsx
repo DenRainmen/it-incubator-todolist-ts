25326d5342594a43308f5e17455881f9c3473ffb
DenRainmen <64159411+DenRainmen@users.noreply.github.com>
2022-07-19 22:49:22 +0300

    First lesson (Victor) Первый урок по Todolist , ментор - Виктор.

---
+++ src/Todolist.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+// здесь типизация props
+export type TodolistType = {
+  title: string;
+  tasks: TaskType[];
+};
+//----end---
+
+// типизация массива Task
+export type TaskType = {
+  id: number;
+  checkbox: boolean;
+  title: string;
+};
+//---end---
+
+
+
+export const Todolist: React.FC<TodolistType> =(props)=> {
+
+  
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        {props.tasks.map((t) => {
+          return (
+            <li key={t.id}>
+              <button >X</button>
+              <input type="checkbox" checked={t.checkbox} />
+              <span>{t.title}</span>
+            </li>
+          );
+        })}
+
+        {/* <li>
+          <input type="checkbox" checked={props.tasks[0].checkbox} />
+          <span>{props.tasks[0].title}</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={true} />
+          <span>JS</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={false} />
+          <span>React</span>
+        </li> */}
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+};
