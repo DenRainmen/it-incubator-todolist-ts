53e16cd1eac3147f27e37eb34af5865ab361d088
Den <potarykindenis@gmail.com>
2022-07-20 19:05:57 +0300

    Установил типизацию для react-router-dom коммандой yarn add @types/react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
 import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Todolist } from "./Todolist";
 
 
 
+
 function App() {
 
-    let [tasks, setTasks] = useState([
-        { id: 1, checkbox: true, title: "HTML" },
-        { id: 2, checkbox: true, title: "CSS" },
-        { id: 3, checkbox: false, title: "JS/TS" },
-        { id: 4, checkbox: false, title: "ReactJS" },
-        { id: 5, checkbox: false, title: "Redux" },
-    ])
+  let [tasks, setTasks] = useState([
+    { id: 1, checkbox: true, title: "HTML" },
+    { id: 2, checkbox: true, title: "CSS" },
+    { id: 3, checkbox: false, title: "JS/TS" },
+    { id: 4, checkbox: false, title: "ReactJS" },
+    { id: 5, checkbox: false, title: "Redux" },
+  ])
 
-  const removeTask = (elId: number) => { 
+  const removeTask = (elId: number) => {
     tasks = tasks.filter(el => el.id !== elId)
     setTasks(tasks)
   }
 
   return (
-    <div className="App">
-          <Todolist
-        title={"What to learn"}
-        tasks={tasks}
-        removeTask={removeTask}
-          />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Todolist
+          title={"What to learn"}
+          tasks={tasks}
+          removeTask={removeTask}
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
